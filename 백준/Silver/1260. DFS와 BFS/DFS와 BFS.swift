e14de98ebd3e2b71392a9931d180246bbb5e4f23
leewanjae <dhkswo1@gmail.com>
2023-12-14 16:31:12 +0900

    Update DFS와 BFS.swift

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.swift
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.swift
@@ -5,51 +5,80 @@
 //  Created by 이완재 on 12/14/23.
 //
 
-let input = readLine()!.split(separator: " ").map { Int($0)! }
-let n = input[0], m = input[1], v = input[2]
-var visited = [Bool](repeating: false, count: n + 1)
-var graph = [[Int]](repeating: [], count: n + 1)
+let inputs = readLine()!.split(separator: " ").map(){Int(String($0))!}
+
+let n = inputs[0]
+let m = inputs[1]
+let v = inputs[2]
+
+var graph = [[Int]](repeating: [], count: n)
+
+graph.append([])
 
 for _ in 0..<m {
-    let input = readLine()!.split(separator: " ").map { Int($0)! }
-    let a = input[0], b = input[1]
-    graph[a].append(b)
-    graph[b].append(a)
+    let arr = readLine()!.split(separator: " ").map(){Int(String($0))!}
+    graph[arr[0]].append(arr[1])
+    graph[arr[0]] = graph[arr[0]].sorted(by: <)
+    graph[arr[1]].append(arr[0])
+    graph[arr[1]] = graph[arr[1]].sorted(by: <)
+    
 }
 
-graph = graph.map { $0.sorted() }
+var visited = [Bool](repeating: false, count: n+1)
 
-func dfs(node: Int) {
-    visited[node] = true
-    print(node, terminator: " ")
-    
-    for nextNode in graph[node] {
-        if !visited[nextNode] {
-            dfs(node: nextNode)
+
+func dfs(_ start: Int) {
+    visited[start] = true
+    print(start, terminator: " ")
+    for i in graph[start] {
+        if !visited[i] {
+            dfs(i)
         }
     }
-    
 }
 
-func bfs(node: Int) {
-    var queue = [node]
-    var index = 0
-    visited[node] = true
-    while queue.count > index {
-        let currentNode = queue[index]
-        print(currentNode, terminator: " ")
-        
-        for nextNode in graph[currentNode] {
-            if !visited[nextNode] {
-                visited[nextNode] = true
-                queue.append(nextNode)
+
+
+// Queue 구현
+public struct Queue<T> {
+    fileprivate var queue: [T] = []
+    
+    public var count: Int {
+        return queue.count
+    }
+    
+    public var isEmpty: Bool {
+        return queue.isEmpty
+    }
+    
+    public mutating func enqueue(_ element: T) {
+        return queue.append(element)
+    }
+    
+    public mutating func dequeue() -> T? {
+        return isEmpty ? nil : queue.removeFirst()
+    }
+}
+
+
+var queue = Queue<Int>()
+
+func bfs(_ start: Int) {
+    queue.enqueue(start)
+    visited[start] = true
+    while(!queue.isEmpty) {
+        guard let now = queue.dequeue() else {return}
+        print(now, terminator: " ")
+        for i in graph[now] {
+            if !visited[i] {
+                queue.enqueue(i)
+                visited[i] = true
             }
         }
-        index += 1
     }
 }
 
-dfs(node: v)
-visited = [Bool](repeating: false, count: n + 1)
+dfs(v)
+visited = [Bool](repeating: false, count: n+1)
 print()
-bfs(node: v)
+bfs(v)
