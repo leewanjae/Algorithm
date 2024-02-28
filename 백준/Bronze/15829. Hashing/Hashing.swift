a4b1e235786fc49934c8334269da2a777f6c050d
leewanjae <dhkswo1@gmail.com>
2024-02-28 21:26:19 +0900

    [Bronze II] Title: Hashing, Time: 12 ms, Memory: 79508 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.swift
+++ 백준/Bronze/15829. Hashing/Hashing.swift
@@ -1,29 +1,21 @@
 import Foundation
 
-// 소문자 a~z // 1~26
-// abba => 1 2 2 1
-
-// 소문자를 숫자로 변환한거 * 31의 제곱
-// abcde
-// 1 * 31의 0승 -> 31
-// 2 * 31의 1승 -> 62
-// 3 * 31의 2승 -> 2883
-// 4 * 31의 3승 -> 119164
-// 5 * 31의 4승 -> 4617605
-let alpha: [Character] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
-let n = Int(readLine()!)!
-var str = readLine()!
-var scores = [Int]()
+let mod = 1234567891
 var result = 0
+let L = Int(readLine()!)! // 문자열의 길이
+let str = readLine()! // 입력받은 문자열
 
-for char in str {
-    scores.append(contentsOf: alpha.enumerated().filter { $0.element == char }.map { $0.offset + 1 })
-}
-
-for (index, element) in scores.enumerated() {
-    result += element * Int(pow(31, Double(index)))
+// 문자열을 순회하며 해시 값을 계산
+for (i, char) in str.enumerated() {
+    let value = Int(char.asciiValue! - Character("a").asciiValue!) + 1
+    var exponent = 1
+    
+    // r의 i 거듭제곱을 모듈러 연산 적용하여 계산
+    for _ in 0..<i {
+        exponent = (exponent * 31) % mod
+    }
+    
+    result = (result + (value * exponent) % mod) % mod
 }
 
 print(result)
-
